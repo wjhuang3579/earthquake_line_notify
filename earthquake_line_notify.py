8a7cfb917743a06f6a144cb0d707fdadcb30c022
wjhuang3579 <wjhuang3579@gmail.com>
2022-03-23 11:57:26 +0800

    git init.

---
+++ earthquake_line_notify.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import os
+import requests
+import argparse
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument("level")
+parser.add_argument("seconds")
+args = parser.parse_args()
+
+earthquake_level = args.level
+earthquake_seconds = args.seconds
+t_future = time.time() + float(earthquake_seconds)
+
+message = '震度 ' + earthquake_level + ' 級，將於 ' + \
+    time.strftime("%I 點 %M 分", time.localtime(t_future)) + ' 抵達。'
+
+
+def lineNotifyMessage(_token, _msg):
+
+    headers = {
+        "Authorization": "Bearer " + _token,
+        "Content-Type": "application/form-data"
+    }
+
+    payload = {
+        'message': _msg
+    }
+
+    r = requests.post("https://notify-api.line.me/api/notify",
+                      headers=headers,
+                      params=payload
+                      )
+    return r.status_code
+
+
+if __name__ == "__main__":
+    __token__ = MYTOKEN
+    lineNotifyMessage(__token__, message)
---
+++ earthquake_start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+python3 earthquake_line_notify.py $1 $2
+# echo $1
+# echo $2
